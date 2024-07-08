ef7742dc10d662fa6acf8bbce99f1481a956744a
8ad40n <joy184110@gmail.com>
2024-07-09 04:11:22 +0600

    Loading spinner

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -3,18 +3,29 @@ import Book from "../Book/Book";
 
 export default function Books() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("books.json")
       .then((response) => response.json())
-      .then((json) => setBooks(json));
+      .then((json) => {
+        setBooks(json);
+        setLoading(false);
+      });
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mx-auto mt-10">
-        {
-            books.map((book) => <Book key={book.id} book={book}></Book>)
-        }
-    </div>
-  )
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mx-auto mt-10">
+        {books.map((book) => (
+          <Book key={book.id} book={book}></Book>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <span
+          className={`${loading ? "loading loading-spinner loading-lg" : ""}`}
+        ></span>
+      </div>
+    </>
+  );
 }
